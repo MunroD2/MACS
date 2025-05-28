0c3c180e81b467a94f9ce5b3ed0e80771fde5870
Shane Gadsby <Shane.Gadsby@unisq.edu.au>
2025-05-28 16:46:56 +1000

    Added all new content added an activity reset button Staging before trying to add second card sort activity

--- sys/js/main.js
+++ sys/js/main.js
@@ -11,13 +11,19 @@ angular.module('Mental Activity Cards', [])
 			print()
 		}
 
+		m.reload = () => {
+			if (confirm("Are you sure you want to reset the activity? You will lose any changes you have made if you proceed.")) {
+				location.reload()
+			}
+		}
+
 		m.cards = [
 			{
 				title: `Deciding`,
 				description: `Debating what to do, what is for the best`,
 				image: `pexels-mizunokozuki-12911860.jpg`,
 				colour: `#eb7c31`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -25,7 +31,7 @@ angular.module('Mental Activity Cards', [])
 				description: `Some event, period or relationship`,
 				image: `pexels-ron-lach-9849635.jpg`,
 				colour: `#a5a5a5`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -33,7 +39,7 @@ angular.module('Mental Activity Cards', [])
 				description: `Working out what matter most, next, or at all to you`,
 				image: `pexels-mikhail-nilov-6592364.jpg`,
 				colour: `#fd0000`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -41,7 +47,7 @@ angular.module('Mental Activity Cards', [])
 				description: `Sorting out what you think about some issue, person or problem`,
 				image: `clarifying-v2.jpg`,
 				colour: `#a8cf8d`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -49,7 +55,7 @@ angular.module('Mental Activity Cards', [])
 				description: `The future, including ‘what would happen if…”`,
 				image: `pexels-bertellifotografia-1830252.jpg`,
 				colour: `#6f309f`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -57,7 +63,7 @@ angular.module('Mental Activity Cards', [])
 				description: `Held with people known to you or whom you know of`,
 				image: `imaginary-conversations-v2.png`,
 				colour: `#00afee`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -65,15 +71,15 @@ angular.module('Mental Activity Cards', [])
 				description: `Estimating whether or not you can afford to do something in terms of money, time or effort`,
 				image: `pexels-cottonbro-3943723.jpg`,
 				colour: `#fdbe00`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
 				title: `Planning`,
-				description: `Day, Week, Year`,
+				description: `The day, the week or much longer ahead.`,
 				image: `pexels-n-voitkevich-8927463.jpg`,
 				colour: `#4371c2`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -81,15 +87,15 @@ angular.module('Mental Activity Cards', [])
 				description: `Practicing what you will say or do`,
 				image: `rehearsing-v2.png`,
 				colour: `#d2349e`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
 				title: `Mulling Over`,
-				description: `Dwelling on the problem`,
+				description: `Dwelling upon a problem, a situation or a relationship.`,
 				image: `mulling-over.png`,
 				colour: `#538135`,
-				authorToReference: "(Munro, 2024)",
+				authorToReference: "(Archer, 2003)",
 				references: "Archer, M.S. (2003). <i>Structure, agency, and the internal conversation.</i> Cambridge University Press."
 			},
 			{
@@ -97,80 +103,80 @@ angular.module('Mental Activity Cards', [])
 				description: `“Knowing” what the right thing to do is.`,
 				image: `pexels-supushpitha-1736222.jpg`,
 				colour: `#8d0000`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Considering`,
 				description: `Taking into account the experiences of others.`,
 				image: `pexels-bertellifotografia-7510729.jpg`,
 				colour: `#4ff2fa`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Comparing`,
 				description: `Weighing your own experience alongside the experience of another person.`,
 				image: `pexels-olia-danilevich-8145356.jpg`,
 				colour: `#caeafd`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Weighing up`,
 				description: `Best case/worst case scenarios.`,
 				image: `pexels-thirdman-8012227.jpg`,
 				colour: `#d0b0df`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Downplaying`,
 				description: `Minimising experiences and expectations.`,
 				image: `downplaying.png`,
 				colour: `#c1cec0`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Praying`,
 				description: `Talking with God about the issue before you.`,
 				image: `praying-v2.jpg`,
 				colour: `#9898fd`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Meditating`,
 				description: `Considering future options in the light of one’s faith and spiritual calling.`,
 				image: `meditating.png`,
 				colour: `#b3c5e5`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Worrying`,
 				description: `Feeling anxious/concerned about present and future plans.`,
 				image: `pexels-mikhail-nilov-7534778.jpg`,
 				colour: `#998fc0`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Procrastinating`,
 				description: `Putting off taking action/making decisions..`,
 				image: `pexels-anntarazevich-6173669.jpg`,
 				colour: `#b76be0`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440."
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]"
 			},
 			{
 				title: `Validating`,
-				description: `Mentally affirming that your judgment is sound and that your decisions, feelings and/or actions are justified.`,
+				description: `Mentally affirming that your judgement is sound and that your decisions, feelings and/or actions are justified.`,
 				image: `validating-v2.png`,
 				colour: `#88c0c3`,
-				authorToReference: "(Munro, 2024)",
-				references: "Munro, D., Willis, J., Gibson, A., & Laundon, M. (2024). From inside the head to putting it on the table–supporting reflexive decision-making for unpaid female carers considering higher education. <i>Reflective Practice, 25</i>(3), 426-440.",
+				authorToReference: "(Munro, 2023)",
+				references: "Munro, D. (2023). <i>How are women with unpaid caring responsibilities making decisions making decisions about higher education study?</i> [Doctoral dissertation, Queensland University of Technology]",
 			},
 		]
 
@@ -195,21 +201,21 @@ angular.module('Mental Activity Cards', [])
 		m.showInstructions = () => {
 			m.showCard({
 				title: `Instructions`,
-				description: `INSTRUCTION TEXT TO GO HERE`,
+				page: `instructions`,
 			})
 		}
 
 		m.showAcknowledgements = () => {
 			m.showCard({
 				title: `Acknowledgements`,
-				description: `ACKNOWLEDGEMENTS TEXT TO GO HERE`,
+				page: `acknowledgements`,
 			})
 		}
 
 		m.showResources = () => {
 			m.showCard({
-				title: `Resources`,
-				description: `RESOURCES TEXT TO GO HERE`,
+				title: `References`,
+				page: `references`,
 			})
 		}
 
